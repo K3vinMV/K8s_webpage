7f8d66fcd0bc005022c42673ccb58db7ddc8a66d
Kevin Melendez <kevinmelendez3535@gmail.com>
2024-10-10 20:24:19 -0600

    template implementado

--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-//Route::resource('productos', ProductoController::class);
+Route::resource('productos', ProductoController::class);
 
 // Admin 
 //Route::resource('admin/productos', ProductoController::class)->middleware('auth'); 
 
 // Cliente
-Route::get('/productos', [ClienteProductoController::class, 'index'])->name('productos.clientes');
+//Route::get('/productos', [ClienteProductoController::class, 'index'])->name('productos.clientes');
 
 // Rutas temporales
 
