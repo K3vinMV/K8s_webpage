b4af492f7303b77ca409d8f4f6a193cf92790172
Kevin Melendez <kevinmelendez3535@gmail.com>
2024-10-11 23:35:47 -0600

    index principal actualizado

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,39 +1,14 @@
 @extends('layouts.template')
 
 @section('content')
-    <!-- Banner Principal -->
-    <div id="bannerPrincipal" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="{{ asset('images/banner1.jpg') }}" class="d-block w-100" alt="Promoción 1">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Bienvenido a Tienda Tenis&Pádel</h5>
-                    <p>Encuentra todo lo que necesitas para tu juego.</p>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="{{ asset('images/banner2.jpg') }}" class="d-block w-100" alt="Promoción 2">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Ofertas Especiales</h5>
-                    <p>Descuentos en raquetas y accesorios seleccionados.</p>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="{{ asset('images/banner3.jpg') }}" class="d-block w-100" alt="Promoción 3">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Nuevas Llegadas</h5>
-                    <p>Explora nuestros nuevos productos de pádel.</p>
-                </div>
-            </div>
+    <!-- Introducción -->
+    <div class="container mt-5">
+        <div class="text-center">
+            <h1 class="display-4">¡Bienvenido a <strong>Game Set Match!</strong></h1>
+            <p class="lead">Tu tienda online especializada en todo lo necesario para el tenis y el pádel. Aquí encontrarás las mejores raquetas, pelotas, accesorios y mucho más. Estamos dedicados a ofrecerte productos de calidad para que lleves tu juego al siguiente nivel.</p>
+            <hr class="my-4">
+            <p>Descubre nuestras categorías destacadas y productos exclusivos para hacer de tu experiencia deportiva algo inigualable.</p>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#bannerPrincipal" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Anterior</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#bannerPrincipal" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Siguiente</span>
-        </button>
     </div>
 
     <!-- Productos Destacados -->
@@ -42,12 +17,12 @@
         <div class="row">
             @foreach ($productosDestacados as $producto)
                 <div class="col-md-4 mb-4">
-                    <div class="card h-100">
+                    <div class="card h-100 shadow">
                         <img src="{{ asset('storage/' . $producto->imagen) }}" class="card-img-top" alt="{{ $producto->nombre }}">
                         <div class="card-body">
                             <h5 class="card-title">{{ $producto->nombre }}</h5>
                             <p class="card-text">${{ number_format($producto->precio, 2) }}</p>
-                            {{-- <a href="{{ route('productos.show', $producto->id) }}" class="btn btn-primary">Ver Detalles</a> --}}
+                            <a href="{{ route('productos.show', $producto->id) }}" class="btn btn-primary">Ver Detalles</a>
                         </div>
                     </div>
                 </div>
@@ -59,8 +34,8 @@
     <div class="container mt-5">
         <h2 class="text-center mb-4">Categorías</h2>
         <div class="row">
-            <div class="col-md-3 mb-4">
-                <div class="card text-center">
+            <div class="col-md-4 mb-4">
+                <div class="card text-center shadow">
                     <img src="{{ asset('images/categoria_raquetas.jpg') }}" class="card-img-top" alt="Raquetas">
                     <div class="card-body">
                         <h5 class="card-title">Raquetas</h5>
@@ -68,17 +43,8 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-3 mb-4">
-                <div class="card text-center">
-                    <img src="{{ asset('images/categoria_pelotas.jpg') }}" class="card-img-top" alt="Pelotas">
-                    <div class="card-body">
-                        <h5 class="card-title">Pelotas</h5>
-                        <a href="#" class="btn btn-secondary">Ver Pelotas</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-3 mb-4">
-                <div class="card text-center">
+            <div class="col-md-4 mb-4">
+                <div class="card text-center shadow">
                     <img src="{{ asset('images/categoria_accesorios.jpg') }}" class="card-img-top" alt="Accesorios">
                     <div class="card-body">
                         <h5 class="card-title">Accesorios</h5>
@@ -86,8 +52,8 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-3 mb-4">
-                <div class="card text-center">
+            <div class="col-md-4 mb-4">
+                <div class="card text-center shadow">
                     <img src="{{ asset('images/categoria_padel.jpg') }}" class="card-img-top" alt="Pádel">
                     <div class="card-body">
                         <h5 class="card-title">Pádel</h5>
@@ -96,5 +62,11 @@
                 </div>
             </div>
         </div>
+    </div>
+
+    <!-- Llamado a la acción -->
+    <div class="container mt-5 text-center">
+        <h3 class="mb-4">¿Listo para mejorar tu juego?</h3>
+        <a href="{{ route('productos.index') }}" class="btn btn-lg btn-success">Explora todo nuestro catálogo</a>
     </div>
 @endsection
